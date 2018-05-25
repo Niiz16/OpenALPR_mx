6a53af9fd776356e680063a80348b3aaa2528238
Denisse <niizhe16@gmail.com>
2018-05-25 15:19:53 -0500

    Modified the module documentation

--- Module_OpenALPR/Module_OpenALPR.py
+++ Module_OpenALPR/Module_OpenALPR.py
@@ -1,3 +1,5 @@
+''' Modulo pensado para recibir el path de una imagen y retornar los patrones posibles, hecho con OpenALPR '''
+
 from openalpr import Alpr
 
 COUNTRY_CODE = 'mx2' 
@@ -6,14 +8,33 @@ CONF_PATH = '/usr/share/openalpr/runtime_data/config/mx2.conf'
 RUNTIME_PATH = '/usr/share/openalpr/runtime_data/' 
 
 def is_correct_plate(candidate):
-    ''' Verifica que las placas coincidan con el archivo de patrones que configuramos.'''
+    ''' Verifica que las placas coincidan con el archivo de patrones que configuramos anteriormente.
+    
+    Parameters
+    ----------
+    candidate:   
+    Recibe un diccionario que confirma o no si la placa esta dentro de nuestro archivo de patrones del pais.
+
+    Returns
+    -------
+    De salida nos entrega un diccionario con la placa y el porcentaje de coincidencia'''
 
     if candidate['matches_template']:
         return { 'plate': candidate['plate'].encode('utf-8'), 'confidence': candidate['confidence'] }
 
 
 def get_candidates(plate):
-    ''' Obtiene todos los patrones candidatos/posibles de la placa '''
+    ''' Obtiene todos los patrones candidatos/posibles de la placa. 
+
+    Parameters
+    ----------
+    plate:
+    Recibe un diccionario que contiene los patrones candidatos.
+
+    Returns
+    -------
+     De salida nos entrega una lista de diccionarios con los patrones posibles y su porcentaje de coincidencia
+    '''
 
     correct_plates = []
     for candidate in plate['candidates']:
@@ -22,7 +43,19 @@ def get_candidates(plate):
 
 
 def numplate(image_path, num_coincidence = 10):
-    ''' Retorna los patrones encontrados en la imagen que se ingresa y que concuerdan con el archivo de patrones que configuramos anteriormente'''
+    ''' Retorna los patrones encontrados en la imagen que se ingresa y que concuerdan con el archivo de patrones que configuramos anteriormente.
+    Parameters
+    ----------
+    image_path:
+    Recibe el path de la imagen a procesar (obligatorio) 
+
+    num_coincidence:
+    El numero de patrones posibles que deseamos analizar (no es obligatorio, por default es 10)
+
+    Returns
+    -------
+    De salida nos entrega una lista con diccionarios con los patrones posibles y su porcentaje de coincidencia
+'''
 
     alpr = Alpr(COUNTRY_CODE, CONF_PATH, RUNTIME_PATH)
     if not alpr.is_loaded():
@@ -49,4 +82,4 @@ if __name__ == '__main__':
         print(result)
 
     else:
-        print("Ejemplo de uso: python Module_OpenALPR.py 1.jpg 10 \nDonde 1.jpg (Campo obligatorio) es el path de la imagen, y 10 (este campo no es obligatorio, por default esta en 10) es el numero de patrones posibles que se analizaran y entregara unicamente los que coincidan con la configuracion de patrones del pais")
+        print("Ejemplo de uso\n-------------- \n>> python Module_OpenALPR.py 1.jpg 10\n \nParameters\n----------\n image_path: \n Recibe el path de la imagen a procesar (obligatorio)\n\n num_coincidence:\n El numero de patrones posibles que deseamos analizar (no es obligatorio, por default es 10)\n\nReturns\n-------\n De salida nos entrega una lista con diccionarios con los patrones posibles y su porcentaje de coincidencia")
